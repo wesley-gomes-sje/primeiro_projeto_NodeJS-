ea83469be5be2e59c1649aa194e504d2cf6f54f5
wesley-gomes-sje <57814784+wesley-gomes-sje@users.noreply.github.com>
2022-02-06 21:27:45 -0300

    inserir deposito

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ const { v4: uuidv4 } = require("uuid")
 const app = express();
 app.use(express.json());
 const customers = [];
+
+//Middleware
+function verifyIfExistsAccountCPF(request, response,next){
+    const { cpf } = request.headers;
+    const customer = customers.find(customer => customer.cpf === cpf);
+    if (!customer){
+        return response.status(400).json({error: "Usuario nao existe"})
+    }
+
+    request.customer = customer;
+    return next();
+}
 app.post("/account", (request, response) =>{
     const {cpf, name} = request.body;
     const customerAlreadyExists = customers.some(
@@ -22,9 +34,22 @@ app.post("/account", (request, response) =>{
     });
     return response.status(201).send();
 });
-app.get("/statement/:cpf", (request, response) =>{
-    const { cpf } = request.params;
-    const customer = customers.find(customer => customer.cpf === cpf);
+
+app.get("/statement", verifyIfExistsAccountCPF,  (request, response) =>{
+    const { customer } = request;
     return response.json(customer.statement);
 })
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request,response) =>{
+    const { description, amount} = request.body;
+    const { customer } = request;
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+    customer.statement.push(statementOperation);
+    return response.status(201).send();
+});
  app.listen(3333);
